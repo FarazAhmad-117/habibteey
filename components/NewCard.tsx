c48f32d6c6c3b033e50e325cc9aa8e529e230aeb
FarazAhmad-117 <farazahmad31048@gmail.com>
2024-10-13 02:56:55 -0700

    done

--- components/NewCard.tsx
+++ components/NewCard.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { CanvasRevealEffect } from "./ui/canvas-reveal-effect";
 
 interface CardProps {
   backText: string;
@@ -11,11 +10,7 @@ const NewCard = ({ backText, backTitle, frontText }: CardProps) => {
   return (
     <div className="container max-w-[200px] max-h-[250px]">
       <div className="card">
-        <div className="front bg-black items-center flex justify-center overflow-hidden">
-          <CanvasRevealEffect
-            animationSpeed={5.1}
-            containerClassName="bg-emerald-900"
-          />
+        <div className="front  bg-gradient-to-t from-black to-slate-800 items-center flex justify-center overflow-hidden">
           <div className="absolute w-full h-full flex items-center justify-center">
             <h2 className="text-3xl text-white">{frontText}</h2>
           </div>
